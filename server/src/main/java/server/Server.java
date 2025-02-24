2722ca30705d6a59efeb2886d89db596a84effcf
Caleb Drake <cadragon11@gmail.com>
2025-02-24 15:56:11 -0700

    Added initial endpoint code

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,12 +10,48 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::loginUser);
+        Spark.delete("/session", this::logoutUser);
+        Spark.get("/game", this::listGame);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+        Spark.delete("/db", this::clearDatabase);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+
+    private Object registerUser(Request request, Response response) {
+        return null;
+    }
+
+    private Object loginUser(Request request, Response response) {
+        return null;
+    }
+
+    private Object logoutUser(Request request, Response response) {
+        return null;
+    }
+
+    private Object listGame(Request request, Response response) {
+        return null;
+    }
+
+    private Object createGame(Request request, Response response) {
+        return null;
+    }
+
+    private Object joinGame(Request request, Response response) {
+        return null;
+    }
+
+    private Object clearDatabase(Request request, Response response) {
+        return null;
     }
 
     public void stop() {
