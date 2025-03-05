6848e4751896230325fe885d2b7e3cc9de9f8e52
Caleb Drake <cadragon11@gmail.com>
2025-03-04 21:59:02 -0700

    Implemented MemoryUserDataAccess

--- server/src/main/java/dataaccess/MemoryUserDataAccess.java
+++ server/src/main/java/dataaccess/MemoryUserDataAccess.java
@@ -6,23 +6,34 @@ import Models.UserData;
 import chess.ChessGame;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 public class MemoryUserDataAccess implements UserDataAccess {
+    Collection<UserData> userDatabase;
+
+    MemoryUserDataAccess() {
+        userDatabase = new HashSet<UserData>();
+    }
     /**
      * UserData methods
      */
     public UserData getUserData(String username) {
-
+        for (UserData user : userDatabase) {
+            if (user.username().equals(username)) {
+                return user;
+            }
+        }
         return null;
     }
-    public void addUserData(UserData userData) {
 
+    public void addUserData(UserData userData) {
+        userDatabase.add(userData);
     }
 
     /**
      * Mass deletion methods
      */
     public void clearUsers() {
-
+        userDatabase.clear();
     }
 }
