f096bf1965051d761103a392b78c8e5effdd9e02
CaDragon1 <cadragon11@gmail.com>
2025-04-08 00:00:46 -0600

    Created ResponseException, started ServerFacade

---
+++ shared/src/main/java/exception/ResponseException.java
@@ -0,0 +1,33 @@
+package exception;
+
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This exception class is similar to the ServerException server class, but with additional code to convert to/from Json
+ */
+public class ResponseException extends Exception {
+    final private int statusCode;
+
+    public ResponseException(String message, int code) {
+        super(message);
+        statusCode = code;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(Map.of("message", getMessage(), "status", statusCode));
+    }
+
+    public ResponseException fromJson(InputStream inputStream) {
+        var map = new Gson().fromJson(new InputStreamReader(inputStream), HashMap.class);
+
+        int code = ((Double) map.get("status")).intValue();
+        String message = map.get("message").toString();
+        
+        return new ResponseException(message, code);
+    }
+}
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,10 +1,24 @@
 package server;
 
+import models.AuthTokenData;
+import models.UserData;
+import exception.ResponseException;
+
 public class ServerFacade {
+
     private final String serverUrl;
+
     public ServerFacade(String url) {
         serverUrl = url;
     }
 
-    public void registerUser()
+    public AuthTokenData registerUser(UserData userData) {
+
+        var path = "/user";
+        return this.makeRequest("POST", );
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+
+    }
 }
