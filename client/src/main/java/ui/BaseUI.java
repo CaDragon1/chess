07b2a146b95fd16de0596c741e7dad77612c951a
Caleb Drake <cadragon11@gmail.com>
2025-04-15 18:27:29 -0600

    Removed REPL in favor of UI-controlled run loops

--- client/src/main/java/ui/BaseUI.java
+++ client/src/main/java/ui/BaseUI.java
@@ -3,11 +3,16 @@ package ui;
 import client.ChessClient;
 import exception.ResponseException;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public abstract class BaseUI implements UIState{
     protected final ChessClient client;
     public UIStatesEnum state;
+    private Scanner scanner;
 
     public BaseUI(ChessClient client) {
+        scanner = new Scanner(System.in);
         this.client = client;
     }
 
@@ -18,5 +23,29 @@ public abstract class BaseUI implements UIState{
         else if (params.length > expectedLength) {
             throw new ResponseException("Too many parameters given", 400);
         }
+    }
+
+    public void run() {
+        boolean keepRunning = true;
+        System.out.println("Welcome to Minecraft, I am Steve\nType a command here! Type 'help' for a list of commands.");
+        System.out.print(">");
+
+        // Trim inputs for accuracy
+        while (keepRunning) {
+            String input = scanner.nextLine().trim();
+
+            try {
+                String result = handler(input);
+                // Adding extra commands for user friendliness :D
+                if (result.equalsIgnoreCase("quit") || result.equalsIgnoreCase("exit") ||
+                        result.equalsIgnoreCase("stop") || result.equalsIgnoreCase("back")) {
+                    keepRunning = false;
+                }
+                System.out.println(result);
+            } catch (ResponseException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        scanner.close();
     }
 }
