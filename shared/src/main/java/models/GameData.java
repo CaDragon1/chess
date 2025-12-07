bca331d1c289af1db2bf61a77be701ea49b8241a
Caleb D <cadragon11@gmail.com>
2025-12-07 16:59:21 -0700

    Added gamestatus as a field in GameData

--- shared/src/main/java/models/GameData.java
+++ shared/src/main/java/models/GameData.java
@@ -3,14 +3,16 @@ package models;
 import chess.ChessGame;
 import com.google.gson.Gson;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game, GameStatus status) {
+
+    public enum GameStatus {PREGAME, RESIGNED, LIVE, WHITE_WIN, BLACK_WIN, STALEMATE}
 
     public GameData setWhiteUsername(String whiteUsername) {
-        return new GameData(this.gameID, whiteUsername, this.blackUsername, this.gameName, this.game);
+        return new GameData(this.gameID, whiteUsername, this.blackUsername, this.gameName, this.game, this.status);
     }
 
     public GameData setBlackUsername(String blackUsername) {
-        return new GameData(this.gameID, this.whiteUsername, blackUsername, this.gameName, this.game);
+        return new GameData(this.gameID, this.whiteUsername, blackUsername, this.gameName, this.game, this.status);
     }
 
     public ChessGame getGame() {
